00ed04df5c5bffd820ffa415fd11ce14693aa2a5
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-09-06 07:18:15 +0200

    cleanup

--- mod.ts
+++ mod.ts
@@ -2,6 +2,7 @@ const lookup: string[] = [];
 const revLookup: number[] = [];
 const code: string =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 for (let i: number = 0, l = code.length; i < l; ++i) {
   lookup[i] = code[i];
   revLookup[code.charCodeAt(i)] = i;
@@ -11,18 +12,23 @@ for (let i: number = 0, l = code.length; i < l; ++i) {
 revLookup["-".charCodeAt(0)] = 62;
 revLookup["_".charCodeAt(0)] = 63;
 
-function getLens(b64: string): number[] {
+function getLens(b64: string): [number, number] {
   const len: number = b64.length;
+
   if (len % 4 > 0) {
-    throw new Error("Invalid string. Length must be a multiple of 4");
+    throw new TypeError("Invalid string. Length must be a multiple of 4");
   }
+
   // Trim off extra bytes after placeholder bytes are found
   // See: https://github.com/beatgammit/base64-js/issues/42
   let validLen: number = b64.indexOf("=");
+
   if (validLen === -1) {
     validLen = len;
   }
+
   const placeHoldersLen: number = validLen === len ? 0 : 4 - (validLen % 4);
+
   return [validLen, placeHoldersLen];
 }
 
@@ -36,13 +42,17 @@ function _byteLength(validLen: number, placeHoldersLen: number): number {
 }
 
 export function toUint8Array(b64: string): Uint8Array {
-  let tmp: number;
   const [validLen, placeHoldersLen]: number[] = getLens(b64);
+
   const buf = new Uint8Array(_byteLength(validLen, placeHoldersLen));
+
   // If there are placeholders, only get up to the last complete 4 chars
   const len: number = placeHoldersLen ? validLen - 4 : validLen;
+
+  let tmp: number;
   let curByte: number = 0;
   let i: number;
+
   for (i = 0; i < len; i += 4) {
     tmp =
       (revLookup[b64.charCodeAt(i)] << 18) |
@@ -53,6 +63,7 @@ export function toUint8Array(b64: string): Uint8Array {
     buf[curByte++] = (tmp >> 8) & 0xff;
     buf[curByte++] = tmp & 0xff;
   }
+
   if (placeHoldersLen === 2) {
     tmp =
       (revLookup[b64.charCodeAt(i)] << 2) |
@@ -66,6 +77,7 @@ export function toUint8Array(b64: string): Uint8Array {
     buf[curByte++] = (tmp >> 8) & 0xff;
     buf[curByte++] = tmp & 0xff;
   }
+
   return buf;
 }
 
@@ -80,30 +92,40 @@ function tripletToBase64(num: number): string {
 
 function encodeChunk(buf: Uint8Array, start: number, end: number): string {
   const out: string[] = new Array((end - start) / 3);
+
   for (let i: number = start, curTriplet: number = 0; i < end; i += 3) {
     out[curTriplet++] = tripletToBase64(
       (buf[i] << 16) + (buf[i + 1] << 8) + buf[i + 2]
     );
   }
+
   return out.join("");
 }
 
 export function fromUint8Array(buf: Uint8Array): string {
-  let tmp: number;
   const maxChunkLength: number = 16383; // Must be multiple of 3
+
   const len: number = buf.length;
+
   const extraBytes: number = len % 3; // If we have 1 byte left, pad 2 bytes
+
   const len2: number = len - extraBytes;
+
   const parts: string[] = new Array(
     Math.ceil(len2 / maxChunkLength) + (extraBytes ? 1 : 0)
   );
+
   let curChunk: number = 0;
   let chunkEnd: number;
+
   // Go through the array every three bytes, we'll deal with trailing stuff later
   for (let i: number = 0; i < len2; i += maxChunkLength) {
     chunkEnd = i + maxChunkLength;
     parts[curChunk++] = encodeChunk(buf, i, chunkEnd > len2 ? len2 : chunkEnd);
   }
+
+  let tmp: number;
+
   // Pad the end with zeros, but make sure to not forget the extra bytes
   if (extraBytes === 1) {
     tmp = buf[len2];
@@ -116,5 +138,6 @@ export function fromUint8Array(buf: Uint8Array): string {
       lookup[(tmp << 2) & 0x3f] +
       "=";
   }
+
   return parts.join("");
 }
--- test.ts
+++ test.ts
@@ -1,63 +1,92 @@
 import { test, runIfMain } from "https://deno.land/std/testing/mod.ts";
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrows
+} from "https://deno.land/std/testing/asserts.ts";
 import * as base64 from "./mod.ts";
 
-const enc: TextEncoder = new TextEncoder();
-const dec: TextDecoder = new TextDecoder();
-
-test(function base64WannabeBigData(): void {
-  const big: Uint8Array = new Uint8Array(13 * 1024 * 1024);
-  for (let i: number = 0, l = big.length; i < l; ++i) {
-    big[i] = i % 256;
-  }
-  const b64: string = base64.fromUint8Array(big);
-  const buf: Uint8Array = base64.toUint8Array(b64);
-  assertEquals(buf, big);
-  assertEquals(base64.byteLength(b64), buf.length);
-});
-
-test(function base64HandlesIrregularPadding(): void {
-  const b64: string = "SQ==QU0=";
-  assertEquals(base64.toUint8Array(b64), Uint8Array.from([73]));
-  assertEquals(base64.byteLength(b64), 1);
-});
-
-test(function base64DecodeUrlSafe(): void {
-  const expected: Uint8Array = Uint8Array.from([
-    0xff,
-    0xff,
-    0xbe,
-    0xff,
-    0xef,
-    0xbf,
-    0xfb,
-    0xef,
-    0xff
-  ]);
-  for (const b64 of ["//++/++/++//", "__--_--_--__"]) {
-    const actual: Uint8Array = base64.toUint8Array(b64);
-    assertEquals(actual, expected);
-    assertEquals(base64.byteLength(b64), actual.length);
+test({
+  name: "toUint8Array throws if a base64 string length is not a multiple of 4",
+  fn(): void {
+    assertThrows((): void => {
+      base64.toUint8Array("Qld");
+    }, TypeError);
   }
 });
 
-test(function base64RoundTrip(): void {
-  const checks: string[] = [
-    "a",
-    "aa",
-    "aaa",
-    "hi",
-    "hi!",
-    "hi!!",
-    "sup",
-    "sup?",
-    "sup?!"
-  ];
-  for (const check of checks) {
-    const b64: string = base64.fromUint8Array(enc.encode(check));
+test({
+  name: "round-tripping big data",
+  fn(): void {
+    const big: Uint8Array = new Uint8Array(13 * 1024 * 1024);
+
+    for (let i: number = 0, l = big.length; i < l; ++i) {
+      big[i] = i % 256;
+    }
+
+    const b64: string = base64.fromUint8Array(big);
     const buf: Uint8Array = base64.toUint8Array(b64);
-    assertEquals(check, dec.decode(buf));
+
+    assertEquals(buf, big);
     assertEquals(base64.byteLength(b64), buf.length);
+  }
+});
+
+test({
+  name: "gracefully handles irregular padding",
+  fn(): void {
+    const b64: string = "SQ==QU0=";
+
+    assertEquals(base64.toUint8Array(b64), Uint8Array.from([73]));
+    assertEquals(base64.byteLength(b64), 1);
+  }
+});
+
+test({
+  name: "toUint8Array acts url safe",
+  fn(): void {
+    const expected: Uint8Array = Uint8Array.from([
+      0xff,
+      0xff,
+      0xbe,
+      0xff,
+      0xef,
+      0xbf,
+      0xfb,
+      0xef,
+      0xff
+    ]);
+
+    for (const b64 of ["//++/++/++//", "__--_--_--__"]) {
+      const actual: Uint8Array = base64.toUint8Array(b64);
+
+      assertEquals(actual, expected);
+      assertEquals(base64.byteLength(b64), actual.length);
+    }
+  }
+});
+
+test({
+  name: "base64RoundTrip",
+  fn(): void {
+    const checks: Uint8Array[] = [
+      "a",
+      "aa",
+      "aaa",
+      "hi",
+      "hi!",
+      "hi!!",
+      "sup",
+      "sup?",
+      "sup?!"
+    ].map(new TextEncoder().encode.bind(this));
+
+    for (const check of checks) {
+      const b64: string = base64.fromUint8Array(check);
+      const buf: Uint8Array = base64.toUint8Array(b64);
+
+      assertEquals(check, buf);
+      assertEquals(base64.byteLength(b64), buf.length);
+    }
   }
 });
 
@@ -71,6 +100,7 @@ test(function base64Identity(): void {
     [Uint8Array.from([1, 1, 1]), "AQEB"],
     [Uint8Array.from([0, -73, 23]), "ALcX"]
   ];
+
   for (const [buf, b64] of data) {
     assertEquals(base64.fromUint8Array(buf), b64);
     assertEquals(base64.toUint8Array(b64), buf);
